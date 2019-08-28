f4128a030a8d0f00ef58915b1566a1c231641e2b
Sebastian Brandt <793580+sebbrandt87@users.noreply.github.com>
2019-08-28 12:24:21 +0200

    updated default version

--- dcos_variables.tf
+++ dcos_variables.tf
@@ -10,7 +10,7 @@ variable "dcos_download_url" {
 }
 
 variable "dcos_version" {
-  default     = "1.13.1"
+  default     = "1.13.3"
   description = "Specifies which DC/OS version instruction to use. Options: 1.13.3, 1.12.4, 1.11.10, etc. See dcos_download_path or dcos_version tree for a full list."
 }
 
